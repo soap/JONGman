e7efc45605db81230a7ad8a94a135d9798fffda1
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-01-05 01:40:48 +0700

    Using twitter bootstrap layout

--- source/pkg_jongman/components/com_jongman/site/views/reservation/tmpl/edit.php
+++ source/pkg_jongman/components/com_jongman/site/views/reservation/tmpl/edit.php
@@ -30,70 +30,84 @@ JHtml::_('behavior.keepalive');
 		}
 	}
 </script>
-<div class="edit">
 <form action="<?php echo JRoute::_('index.php?option=com_jongman&layout=edit&id='.(int) $this->item->id); ?>"
 	method="post" name="adminForm" id="reservation-form" class="form-validate form-inline">
-	<div>
-	<div class="formelm-buttons btn-toolbar">
-		<button type="button" class="btn btn-info" onclick="Joomla.submitbutton('reservation.save')">
-			<?php echo JText::_('JSAVE') ?>
-		</button>
-		<button type="button" class="btn btn-info" onclick="Joomla.submitbutton('reservation.cancel')">
-			<?php echo JText::_('JCANCEL') ?>
-		</button>
+	<div class="row-fluid">
+		<div class="span-12">
+			<div class="formelm-buttons btn-toolbar">
+				<button type="button" class="btn btn-info"
+					onclick="Joomla.submitbutton('reservation.save')">
+					<?php echo JText::_('JSAVE') ?>
+				</button>
+				<button type="button" class="btn btn-info"
+					onclick="Joomla.submitbutton('reservation.cancel')">
+					<?php echo JText::_('JCANCEL') ?>
+				</button>
+			</div>
+		</div>
 	</div>
-	<div class="width-60 fltlft">
-		<fieldset class="adminform">
-			<legend><?php echo JText::_('COM_JONGMAN_RESERVATION_TITLE')?></legend>
-    		<div class="formelm control-group">
-                <div class="control-label">
-					<?php echo $this->form->getLabel('title'); ?>
-					<?php echo $this->form->getInput('title'); ?>
+	<div class="row-fluid">
+		<div class="span6 form-horizontal">
+			<legend>
+			<?php echo JText::_('COM_JONGMAN_RESERVATION_TITLE')?>
+			</legend>
+
+			<div class="control-group">
+				<div class="control-label">
+				<?php echo $this->form->getLabel('title'); ?>
+				</div>
+				<div class="controls">
+				<?php echo $this->form->getInput('title'); ?>
 				</div>
 			</div>
 
-    		<div class="formelm control-group">
-                <div class="control-label">
+			<div class="control-group">
+				<div class="control-label">
 					<?php echo $this->form->getLabel('alias'); ?>
+				</div>
+				<div class="controls">
 					<?php echo $this->form->getInput('alias'); ?>
 				</div>
 			</div>
-    		<div class="formelm control-group">
-                <div class="control-label">
+			
+			<div class="control-group">
+				<div class="control-label">
 					<?php echo $this->form->getLabel('resource_id'); ?>
+				</div>
+				<div class="controls">
 					<?php echo $this->form->getInput('resource_id'); ?>
 				</div>
-			</div>			
-    		<div class="formelm control-group">
-                <div class="control-label">
+			</div>
+			<div class="control-group">
+				<div class="control-label">
 					<?php echo $this->form->getLabel('start_date'); ?>
-					<?php echo $this->form->getInput('start_date'); ?>
-				</li>				
-    		<div class="formelm control-group">
-                <div class="control-label">
-					<?php echo $this->form->getLabel('end_date'); ?>
-					<?php echo $this->form->getInput('end_date'); ?>
 				</div>
-			</div>				
-
-
-			<?php echo $this->form->getLabel('description'); ?>
-			<div class="clr"></div>
-			<?php echo $this->form->getInput('description'); ?>
-
-		</fieldset>
-	</div>
-	<div class="width-40 fltrt">
-		<?php //echo JHtml::_('sliders.start','reservation-sliders-'.$this->item->id, array('useCookie' => 1)); ?>
-
-		<?php //echo $this->loadTemplate('params'); ?>
-
-		<?php //echo JHtml::_('sliders.end'); ?>
-
-	</div>
-	<div class="clr"></div>
-	<input type="hidden" name="schedule_id" value="<?php echo $this->item->schedule_id?>>" />
-	<input type="hidden" name="task" value="" />
-	<?php echo JHtml::_('form.token'); ?>
-</form>
-</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('start_date'); ?>
+					<?php echo $this->form->getInput('start_time'); ?>
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('end_date'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('end_date'); ?>
+					<?php echo $this->form->getInput('end_time'); ?>
+				</div>
+			</div>
+			<div class="control-grouop">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('description'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('description'); ?>
+				</div>
+			</div>
+		</div>
+		<div class="span6">
+		</div>
+		<input type="hidden" name="schedule_id" value="<?php echo $this->item->schedule_id?>>" />
+		<input type="hidden" name="task" value="" />
+		<?php echo JHtml::_('form.token'); ?>
+	</form>
