28c62940f18099c1f5b371446627f61e3b988234
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-02-02 16:51:05 +0700

    re-write SQL query

--- source/pkg_jongman/components/com_jongman/site/models/reservations.php
+++ source/pkg_jongman/components/com_jongman/site/models/reservations.php
@@ -22,7 +22,7 @@ class JongmanModelReservations extends JModelList
 	 * @return  void
 	 * @since   1.0
 	 */
-	protected function populateState($ordering = 'r.start_date', $direction = 'asc')
+	protected function populateState($ordering = 'a.start_date', $direction = 'asc')
 	{
 		// Set list state ordering defaults.
 		parent::populateState($ordering, $direction);
@@ -43,31 +43,39 @@ class JongmanModelReservations extends JModelList
 		// Select the required fields from the table.
 		$query->select(
 			$this->getState(
-				'list.select',
-				'r.id, r.checked_out, r.checked_out_time, r.resource_id,' .
-				'r.state, r.created_time'
+				'list.select', 
+				'a.id as instance_id, a.start_date, a.end_date, a.reference_number'
 			)
 		);
-		$query->from('#__jongman_reservations AS r');
+		$query->from('#__jongman_reservation_instances AS a');
+		
+		$query->select('r.alias, r.titile, r.description, ' .
+				'r.checked_out, r.checked_out_time, r.schedule_id, ' .
+				'r.state, r.created_time');		
+		$query->join('INNER','#__jongman_reservations AS r ON r.id=a.reservation_id');
 
 		$query->select('sc.name as schedule_name');
-		$query->join('LEFT', '#__jongman_schedules AS sc ON sc.id=r.schedule_id');
+		$query->join('LEFT', '#__jongman_schedules AS s ON s.id=r.schedule_id');
 		
 		// Join over the users for the checked out user.
 		$query->select('uc.name AS editor');
 		$query->join('LEFT', '#__users AS uc ON uc.id=r.checked_out');
 
-		// Join over the categories.
+		// Join over the users for the author.
+		$query->select('ua.name AS author_name');
+		$query->join('LEFT', '#__users AS ua ON ua.id = r.created_by');
+		
+		// Join over the users for the owner.
+		$query->select('uo.name AS owner_name');
+		$query->join('LEFT', '#__users AS uo ON uo.id = r.owner_id');
+
+		// Join over the resource.
 		$query->select('rs.title AS resource_title, rs.access');
 		$query->join('LEFT', '#__jongman_resources AS rs ON rs.id = r.resource_id');
 		
 		// Join over the asset groups.
 		$query->select('ag.title AS access_level');
-		$query->join('LEFT', '#__viewlevels AS ag ON ag.id = rs.access');
-
-		// Join over the users for the author.
-		$query->select('ua.name AS author_name');
-		$query->join('LEFT', '#__users AS ua ON ua.id = r.created_by');
+		$query->join('LEFT', '#__viewlevels AS ag ON ag.id = r.access');
 
 		// Add the list ordering clause.
 		$orderCol	= $this->state->get('list.ordering');
