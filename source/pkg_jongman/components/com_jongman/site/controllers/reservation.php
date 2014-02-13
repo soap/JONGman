c1e9b8488a31e1049f0499891cada2973731978e
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-02-13 14:43:39 +0700

    Clear layout variable in the request on edit

--- source/pkg_jongman/components/com_jongman/site/controllers/reservation.php
+++ source/pkg_jongman/components/com_jongman/site/controllers/reservation.php
@@ -23,76 +23,10 @@ class JongmanControllerReservation extends JControllerForm
 	
 	public function edit($key = null, $urlVar = null)
 	{
-		// Initialise variables.
-		$app = JFactory::getApplication();
-		$model = $this->getModel();
-		$table = $model->getTable();
-		$cid = $app->input->getVar('cid', array());
-		$context = "$this->option.edit.$this->context";
-
-		// Determine the name of the primary key for the data.
-		if (empty($key))
-		{
-			$key = $table->getKeyName();
-		}
-
-		// To avoid data collisions the urlVar may be different from the primary key.
-		if (empty($urlVar))
-		{
-			$urlVar = $key;
-		}
-
-		// Get the previous record id (if any) and the current record id.
-		$recordId = (int) (count($cid) ? $cid[0] : $app->input->getInt($urlVar));
-		$checkin = property_exists($table, 'checked_out');
-
-		// Access check.
-		if (!$this->allowEdit(array($key => $recordId), $key))
-		{
-			$this->setError(JText::_('JLIB_APPLICATION_ERROR_EDIT_NOT_PERMITTED'));
-			$this->setMessage($this->getError(), 'error');
-
-			$this->setRedirect(
-				JRoute::_(
-					'index.php?option=' . $this->option . '&view=' . $this->view_list
-					. $this->getRedirectToListAppend(), false
-				)
-			);
-
-			return false;
-		}
-
-		// Attempt to check-out the new record for editing and redirect.
-		if ($checkin && !$model->checkout($recordId))
-		{
-			// Check-out failed, display a notice but allow the user to see the record.
-			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_CHECKOUT_FAILED', $model->getError()));
-			$this->setMessage($this->getError(), 'error');
-
-			$this->setRedirect(
-				JRoute::_(
-					'index.php?option=' . $this->option . '&view=' . $this->view_item
-					. $this->getRedirectToItemAppend($recordId, $urlVar), false
-				)
-			);
-
-			return false;
-		}
-		else
-		{
-			// Check-out succeeded, push the new record id into the session.
-			$this->holdEditId($context, $recordId);
-			$app->setUserState($context . '.data', null);
-
-			$this->setRedirect(
-				JRoute::_(
-					'index.php?option=' . $this->option . '&view=' . $this->view_item
-					. $this->getRedirectToItemAppend($recordId, $urlVar), false
-				)
-			);
-
-			return true;
-		}
+		$input = JFactory::getApplication()->input;
+		$input->set('layout', 'edit');
+		return parent::edit($key, $urlVar);
+		
 	}
 	
 	/**
@@ -129,7 +63,21 @@ class JongmanControllerReservation extends JControllerForm
 	
 	protected function allowEdit($data=array(), $key = 'id')
 	{
-		return ReservationHelper::canEdit($data, $key);
+		$owner_id = 0;
+		if (is_array($data) && isset($data['id'])) {
+			$model = $this->getModel();
+			$reservationInstance = $model->getItem($data['id']);
+			$owner_id = $reservationInstance->owner_id;	
+		}
+		$asset = 'com_jongman';
+		$actions = JongmanHelper::getActions($asset);
+		if ($actions->get('core.edit')) return true;
+		
+		if (($owner_id > 0) && $owner_id == JFactory::getUser()->id) {
+			if ($actions->get('core.edit.own')) return true;
+		}
+		
+		return false;
 	} 
 	
 	protected function getRedirectToItemAppend($recordId = null, $urlVar = 'id') 
