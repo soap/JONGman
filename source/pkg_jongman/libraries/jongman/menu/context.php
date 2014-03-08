01d81806bf716344fc8e7a84a5b9eb74c90e4d60
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-03-08 21:29:36 +0700

    Reformat

--- source/pkg_jongman/libraries/jongman/menu/context.php
+++ source/pkg_jongman/libraries/jongman/menu/context.php
@@ -1,15 +1,5 @@
 <?php
-/**
- * @package      Projectfork.Library
- * @subpackage   Menu
- *
- * @author       Tobias Kuhn (eaxs)
- * @copyright    Copyright (C) 2006-2012 Tobias Kuhn. All rights reserved.
- * @license      http://www.gnu.org/licenses/gpl.html GNU/GPL, see LICENSE.txt
- */
-
 defined('_JEXEC') or die();
-
 
 class RFMenuContext
 {
