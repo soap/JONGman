d5bb609292d7fe6ace60742b8667869a74370c54
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-03-09 21:23:39 +0700

    Add reference_number

--- source/pkg_jongman/components/com_jongman/site/views/instanceitem/tmpl/default.php
+++ source/pkg_jongman/components/com_jongman/site/views/instanceitem/tmpl/default.php
@@ -4,7 +4,11 @@ defined('_JEXEC') or die;
 <div class="reservationbox">
 	<div id="reservationDetails">
 		<ul class="unstyle">
-			<?php var_dump($this->item);?>
+			<div class="users"><?php echo $this->item->owner_name?></div>
+			<div class="dates"></div>
+			<div class="referncenumber"><?php echo $this->item->reference_number?></div>
+			<div class="title"><?php echo $this->item->title?></div>
+			<div class="resources"></div>
 		</ul>
 	</div>
 </div>
