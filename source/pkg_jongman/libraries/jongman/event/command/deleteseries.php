99aecf3abf906dfb3d7decedddd8df6fa512286f
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-03-23 13:59:28 +0700

    Progress on event command

--- source/pkg_jongman/libraries/jongman/event/command/deleteseries.php
+++ source/pkg_jongman/libraries/jongman/event/command/deleteseries.php
@@ -1,3 +1,21 @@
 <?php
 defined('_JEXEC') or die;
 
+class RFEventCommandDeleteseries extends RFEventCommand
+{
+	public function __construct(RFReservationExistingseries $series)
+	{
+		$this->series = $series;
+	}
+	
+	public function execute($dbo = null)
+	{
+		if ($dbo == null) $dbo = JFactory::getDbo();
+		$query = $dbo->getQuery(true);
+		$query->delete('#__jongman_reservations')
+			->where('id = '.$this->series->seriesId());
+		$db->setQuery($dbo);
+		return $dbo->execute();
+	}
+}
+
