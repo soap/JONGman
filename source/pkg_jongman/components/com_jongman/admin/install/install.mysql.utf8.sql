c16d544f8327c139d1da00f5ec1a1341eb7a49af
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-03-25 17:26:48 +0700

    Prepare database schema for JONGman 2.0.0-beta.1

--- source/pkg_jongman/components/com_jongman/admin/install/install.mysql.utf8.sql
+++ source/pkg_jongman/components/com_jongman/admin/install/install.mysql.utf8.sql
@@ -1,3 +1,8 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `#__jongman_layouts`
+--
 
 CREATE TABLE IF NOT EXISTS `#__jongman_layouts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -15,15 +20,7 @@ CREATE TABLE IF NOT EXISTS `#__jongman_layouts` (
   `access` int(11) NOT NULL DEFAULT '1',
   `language` varchar(10) NOT NULL DEFAULT '*',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `#__jongman_layouts`
---
-
-INSERT INTO `#__jongman_layouts` (`id`, `title`, `alias`, `timezone`, `published`, `note`, `created`, `created_by`, `modified`, `modified_by`, `checked_out`, `checked_out_time`, `access`, `language`) VALUES
-(1, 'Default', 'default', 'Asia/Bangkok', 1, '', '2013-11-03 11:00:00', 42, '2013-11-26 08:07:31', 42, 0, '0000-00-00 00:00:00', 1, '*'),
-(2, 'Hourly 8-20', 'hourly-8-20', 'Asia/Bangkok', 1, '', '2013-11-28 15:17:13', 42, '0000-00-00 00:00:00', 0, 0, '0000-00-00 00:00:00', 1, '*');
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -54,15 +51,7 @@ CREATE TABLE IF NOT EXISTS `#__jongman_quotas` (
   KEY `resource_id` (`resource_id`),
   KEY `group_id` (`group_id`),
   KEY `schedule_id` (`schedule_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `#__jongman_quotas`
---
-
-INSERT INTO `#__jongman_quotas` (`id`, `title`, `alias`, `quota_limit`, `unit`, `duration`, `schedule_id`, `resource_id`, `group_id`, `checked_out`, `checked_out_time`, `created_user_id`, `created_time`, `modified_user_id`, `modified_time`, `published`, `access`, `note`) VALUES
-(1, 'Limit 2 reservations per day globally', '', '2.00', 'reservation', 'daily', 0, 0, 0, 0, '0000-00-00 00:00:00', 42, '2012-11-05 05:27:12', 42, '2013-06-16 15:39:11', 1, 1, 'Limit 2 reservations per day globally'),
-(2, 'Globally 4 hours daily reservation', '', '240.00', 'minute', 'daily', 0, 0, 0, 0, '0000-00-00 00:00:00', 42, '2012-11-05 07:15:38', 42, '2013-06-16 15:39:24', 1, 1, 'Limit only 4 hours daily reservation');
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -72,36 +61,57 @@ INSERT INTO `#__jongman_quotas` (`id`, `title`, `alias`, `quota_limit`, `unit`, 
 
 CREATE TABLE IF NOT EXISTS `#__jongman_reservations` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `resource_id` int(11) unsigned NOT NULL COMMENT 'resource id',
-  `schedule_id` int(11) unsigned NOT NULL COMMENT 'schedule id',
-  `title` varchar(200) DEFAULT NULL COMMENT 'Title or subject of reservation',
-  `alias` varchar(200) NOT NULL COMMENT 'alias for user reference',
-  `start_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `end_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `created_by` int(11) NOT NULL COMMENT 'reserved by (user id)',
+  `schedule_id` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `alias` varchar(50) NOT NULL,
+  `repeat_type` varchar(10) NOT NULL,
+  `repeat_options` text NOT NULL,
+  `created_by` int(11) unsigned NOT NULL COMMENT 'reserved by (user id)',
   `created` datetime NOT NULL COMMENT 'change from created',
-  `modified_by` int(11) NOT NULL DEFAULT '0',
+  `modified_by` int(11) unsigned NOT NULL DEFAULT '0',
   `modified` datetime DEFAULT '0000-00-00 00:00:00',
-  `reserved_for` int(11) DEFAULT NULL COMMENT 'reserver for (user id)',
-  `is_blackout` smallint(6) NOT NULL DEFAULT '0',
-  `state` smallint(6) NOT NULL DEFAULT '0' COMMENT 'changed from is_pending',
-  `description` text,
-  `allow_participation` smallint(6) NOT NULL DEFAULT '0',
-  `allow_anon_participation` smallint(6) NOT NULL DEFAULT '0',
+  `type_id` tinyint(4) NOT NULL DEFAULT '1' COMMENT '1=reservation,2=black out',
+  `state` smallint(6) NOT NULL DEFAULT '1' COMMENT 'changed from is_pending;1=approved, -1=pending',
   `checked_out` int(11) NOT NULL DEFAULT '0',
   `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `access` int(11) NOT NULL,
+  `access` int(11) NOT NULL DEFAULT '0',
+  `note` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `res_machid` (`resource_id`),
-  KEY `res_scheduleid` (`schedule_id`),
-  KEY `reservations_startdate` (`start_date`),
-  KEY `reservations_enddate` (`end_date`),
   KEY `res_created` (`created`),
   KEY `res_modified` (`modified`),
-  KEY `res_isblackout` (`is_blackout`),
   KEY `reservations_pending` (`state`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Store reservation data' AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Store reservation series' AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `#__jongman_reservation_instances`
+--
+
+CREATE TABLE IF NOT EXISTS `#__jongman_reservation_instances` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `start_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `end_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `reference_number` varchar(20) NOT NULL,
+  `reservation_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='one reservation has more than one instance if its repeat option is not none.' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `#__jongman_reservation_resources`
+--
+
+CREATE TABLE IF NOT EXISTS `#__jongman_reservation_resources` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `reservation_id` int(11) NOT NULL,
+  `resource_id` int(11) NOT NULL,
+  `resource_level` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `reservation_id` (`reservation_id`,`resource_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='multiple resources per reservation' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -113,10 +123,7 @@ CREATE TABLE IF NOT EXISTS `#__jongman_reservation_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `reservation_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `owner` tinyint(4) NOT NULL DEFAULT '0',
-  `can_edit` tinyint(4) NOT NULL DEFAULT '0',
-  `can_delete` tinyint(4) NOT NULL DEFAULT '0',
-  `accept_code` varchar(30) DEFAULT NULL,
+  `user_level` tinyint(4) DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `reservation_id` (`reservation_id`,`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Users in each reservation, owner=1 implies reservation owner' AUTO_INCREMENT=1 ;
@@ -131,6 +138,8 @@ CREATE TABLE IF NOT EXISTS `#__jongman_resources` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `schedule_id` int(11) NOT NULL,
   `title` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(200) NOT NULL,
   `alias` varchar(128) NOT NULL,
   `location` varchar(250) DEFAULT NULL,
   `contact_info` varchar(200) NOT NULL,
@@ -151,6 +160,10 @@ CREATE TABLE IF NOT EXISTS `#__jongman_resources` (
   `access` int(11) NOT NULL DEFAULT '0',
   `ordering` int(11) NOT NULL DEFAULT '0',
   `published` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `created_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_by` int(11) NOT NULL DEFAULT '0',
+  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` int(11) NOT NULL DEFAULT '0',
   `checked_out` int(11) NOT NULL DEFAULT '0',
   `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
@@ -159,6 +172,7 @@ CREATE TABLE IF NOT EXISTS `#__jongman_resources` (
   KEY `rs_status` (`status`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `#__jongman_schedules`
@@ -191,7 +205,6 @@ CREATE TABLE IF NOT EXISTS `#__jongman_schedules` (
   `published` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
 
 -- --------------------------------------------------------
 
--- source/pkg_jongman/components/com_jongman/admin/install/schemas/mysql/0.0.3.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE #__jongman_schedules ADD timezone VARCHAR(50) AFTER time_span;
