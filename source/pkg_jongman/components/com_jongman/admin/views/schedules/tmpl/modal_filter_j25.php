af46b93f369e42ed4e6c4aec5215ebd95426fb95
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-04-02 10:27:56 +0700

    Make them J3.x compatible

---
+++ source/pkg_jongman/components/com_jongman/admin/views/schedules/tmpl/modal_filter_j25.php
@@ -0,0 +1,30 @@
+<?php
+defined('_JEXEC') or die;
+
+$function	= JRequest::getCmd('function', 'jSelectSchedule');
+$listOrder	= $this->escape($this->state->get('list.ordering'));
+$listDirn	= $this->escape($this->state->get('list.direction'));
+?>
+	<fieldset class="filter clearfix">
+		<div class="left">
+			<label for="filter_search"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?></label>
+			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" size="30" title="<?php echo JText::_('COM_JONGMAN_FILTER_SEARCH_SCHEDULE_DESC'); ?>" />
+
+			<button type="submit">
+				<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
+			<button type="button" onclick="document.id('filter_search').value='';this.form.submit();">
+				<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
+		</div>
+
+		<div class="right">
+			<select name="filter_access" class="inputbox" onchange="this.form.submit()">
+				<option value=""><?php echo JText::_('JOPTION_SELECT_ACCESS');?></option>
+				<?php echo JHtml::_('select.options', JHtml::_('access.assetgroups'), 'value', 'text', $this->state->get('filter.access'));?>
+			</select>
+
+			<select name="filter_published" class="inputbox" onchange="this.form.submit()">
+				<option value=""><?php echo JText::_('JOPTION_SELECT_PUBLISHED');?></option>
+				<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.published'), true);?>
+			</select>
+		</div>
+	</fieldset>
