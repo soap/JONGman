1faa86de4de239f890e19eee05ed71ab698a462c
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-04-18 09:03:17 +0700

    Fix error SQL command

--- source/pkg_jongman/components/com_jongman/admin/install/schemas/mysql/0.0.5.sql
+++ source/pkg_jongman/components/com_jongman/admin/install/schemas/mysql/0.0.5.sql
@@ -1,8 +1,8 @@
 ALTER TABLE `#__jongman_resources` ADD COLUMN `auto_assign` tinyint(4) NOT NULL DEFAULT '1',
-  `allow_multi_days` tinyint(4) NOT NULL DEFAULT '0',
-  `max_participants` smallint(6) NOT NULL DEFAULT '0',
-  `min_reservation_duration` int(11) NOT NULL DEFAULT '0',
-  `max_reservation_duration` int(11) NOT NULL DEFAULT '0',
-  `min_notice_duration` int(11) NOT NULL DEFAULT '0',
-  `max_notice_duration` int(11) NOT NULL DEFAULT '0',
-  `requires_approval` tinyint(4) NOT NULL DEFAULT '0' AFTER `contact_info`;
+ADD COLUMN `allow_multi_days` tinyint(4) NOT NULL DEFAULT '0',
+ADD COLUMN `max_participants` smallint(6) NOT NULL DEFAULT '0',
+ADD COLUMN `min_reservation_duration` int(11) NOT NULL DEFAULT '0',
+ADD COLUMN `max_reservation_duration` int(11) NOT NULL DEFAULT '0',
+ADD COLUMN `min_notice_duration` int(11) NOT NULL DEFAULT '0',
+ADD COLUMN `max_notice_duration` int(11) NOT NULL DEFAULT '0',
+ADD COLUMN `requires_approval` tinyint(4) NOT NULL DEFAULT '0' AFTER `contact_info`;
