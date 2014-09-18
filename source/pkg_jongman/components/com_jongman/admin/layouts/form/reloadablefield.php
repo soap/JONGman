18bedc16b871f5723faaa7942f9d03033cde64b6
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-09-19 01:33:17 +0700

    This will be called by JFormFieldReloadable or JFormFieldListReloadable

--- source/pkg_jongman/components/com_jongman/admin/layouts/form/reloadablefield.php
+++ source/pkg_jongman/components/com_jongman/admin/layouts/form/reloadablefield.php
@@ -12,20 +12,19 @@ defined('_JEXEC') or die;
 ?>
 <?php
 
-//if (!empty($displayData['options']['showonEnabled']))
-//{
-//	JHtml::_('jquery.framework');
-//	JHtml::_('script', 'jui/cms.js', false, true);
-//}
+if (!empty($displayData['options']['showonEnabled']))
+{
+	JHtml::_('jquery.framework');
+	JHtml::_('script', 'jui/cms.js', false, true);
+}
 ?>
-
-<div class="control-group <?php echo $displayData->class; ?>" <?php //echo $displayData['options']['rel']; ?>>
-	<?php //if (empty($displayData['options']['hiddenLabel'])) : ?>
-		<div class="control-label"><?php echo $displayData->label; ?></div>
-	<?php //endif; ?>
+<div class="control-group <?php echo $displayData['options']['class']; ?>" <?php echo $displayData['options']['rel']; ?>>
+	<?php if (empty($displayData['options']['hiddenLabel'])) : ?>
+		<div class="control-label"><?php echo $displayData['label']; ?></div>
+	<?php endif; ?>
 	<div class="controls">
-		<div id="<?php echo $displayData->id?>_reload">
-			<?php echo $displayData->input; ?>
+		<div id="<?php echo $displayData['id']?>_reload">
+			<?php echo $displayData['input']; ?>
 		</div>
 	</div>
 </div>
