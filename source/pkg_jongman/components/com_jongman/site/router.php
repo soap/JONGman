e6e3d4ac3d7646fda14d69598e718b15ee3c559f
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2015-03-03 23:59:08 +0700

    Update router.php  for schedule view. Tested.

--- source/pkg_jongman/components/com_jongman/site/router.php
+++ source/pkg_jongman/components/com_jongman/site/router.php
@@ -7,27 +7,51 @@
  */
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die;
-function JongmanBuildRoute( $query )
+function JongmanBuildRoute( &$query )
 {
-    $segments = array();
+   	$segments = array();
+
     if (isset($query['view']) && ($query['view'] != 'reservation') )
     {
-        /** first call alias is ok */
+    	$segments[] = $query['view'];
+		/* We should use slug for schedule id here? */
+    	if (isset($query['id'])) {
+    		$segments[] = $query['id'];
+    		unset($query['id']);
+    	}
+    	
+    	if (isset($query['layout'])) {
+    		$segments[] = $query['layout'];
+    		unset($query['layout']);
+    	}
+    	
+    	if (isset($query['sd'])) {
+    		$segments[] = $query['sd'];
+			unset($query['sd']);
+    	}
         unset($query['view']);
-        unset($query['id']); 
     }
-    
     return $segments;   
 }
 
 function JongmanParseRoute( &$segments )
 {
-    $vars = array();
-    if (count($segments)==1) {
-        list($name, $value) = split(':', $segments[0]);
-        $vars['date']= $value;
-        return $vars;
-    }
+    $vars	= array();
+    $count	= count($segments);
+	switch($segments[0]) {
+		case 'schedule':
+			$vars['view'] 	= 'schedule';
+			$vars['id']		= (int)$segments[1];
+			if (isset($segments[2])) {
+				$vars['layout'] = $segments[2];
+			}
+			
+			if (isset($segments[3])) {
+				/** sd=2015-12-01 converted to 2015:12-01 **/
+				$vars['sd'] = str_replace(':', '-', $segments[3]);
+			}
+			break;
+	}
     
     return $vars;        
 }
