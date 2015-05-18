0bd0e13e8c1d6c7e7e3edf98f4d2ef1f65ecdcf6
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2015-05-18 20:24:31 +0700

    Missing translation strings

--- source/pkg_jongman/components/com_jongman/admin/models/fields/approvalsystem.php
+++ source/pkg_jongman/components/com_jongman/admin/models/fields/approvalsystem.php
@@ -18,13 +18,13 @@ class JFormFieldApprovalSystem extends JFormFieldList
 		// Create a new option object based on the <option /> element.
 		$tmp = JHtml::_(
 				'select.option', '1',
-				JText::_('COM_JONGMAN_JONGMAN_APPROVAL_SYSTEM'), 'value', 'text', false);
+				JText::_('COM_JONGMAN_OPTION_APPROVAL_SYSTEM_JONGMAN'), 'value', 'text', false);
 		$options[] = $tmp;
 		
 		$disabled = !JComponentHelper::isInstalled('com_workflow');
 		$tmp = JHtml::_(
 				'select.option', '2',
-				JText::_('COM_JONGMAN_JWORKFLOW_APPROVAL_SYSTEM'), 'value', 'text', $disabled);
+				JText::_('COM_JONGMAN_OPTION_APPROVAL_SYSTEM_JWORKFLOW'), 'value', 'text', $disabled);
 		$options[] = $tmp;
 		
 		reset($options);
