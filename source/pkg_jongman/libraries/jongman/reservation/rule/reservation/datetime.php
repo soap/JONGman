760356b8c22f22284c9746a4c831ea8f5f65ba1c
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2015-05-18 23:16:20 +0700

    Removed Joomla 's model dependency from JONGman library

--- source/pkg_jongman/libraries/jongman/reservation/rule/reservation/datetime.php
+++ source/pkg_jongman/libraries/jongman/reservation/rule/reservation/datetime.php
@@ -3,21 +3,21 @@ defined('_JEXEC') or die;
 
 class RFReservationRuleReservationDatetime implements IReservationValidationRule
 {
-	private $message = '';
+	private $__message = '';
 	
 	public function validate($reservationSeries)
 	{
 		$instance = $reservationSeries->currentInstance();
 		$startBeforeEnd = $instance->startDate()->lessThan($instance->endDate());
 		if (!$startBeforeEnd) {
-			$this->message = JText::_('COM_JONGMAN_ERROR_STARTDATE_LESSTHAN_ENDDATE');
-			return new RFReservationReuleResult(false, $this->message);
+			$this->__message = JText::_('COM_JONGMAN_ERROR_STARTDATE_LESSTHAN_ENDDATE');
+			return new RFReservationReuleResult(false, $this->__message);
 		}
 		return new RFReservationRuleResult();
 	}
 	
 	public function getError()
 	{
-		return $this->message;
+		return $this->__message;
 	}
 }
