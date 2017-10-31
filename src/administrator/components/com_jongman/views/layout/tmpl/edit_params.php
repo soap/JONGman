efe5ea1c638aeb20a86e75270f5574fc201f516c
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2017-10-31 20:10:26 +0700

    [4.0]Update backend structure

--- src/administrator/components/com_jongman/views/layout/tmpl/edit_params.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* @package     JONGman Package
-*
-* @copyright   Copyright (C) 2005 - 2017 Prasit Gebsaap, Inc. All rights reserved.
-* @license     GNU General Public License version 2 or later; see LICENSE.txt
-*/
-defined('_JEXEC') or die;
-
